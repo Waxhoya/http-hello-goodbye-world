d13e24191dd69454e3a1aa6eca85221287c4725f
Albert Reel <waxhoya@gmail.com>
2016-10-27 11:29:32 -0700

    remove double URL parser

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const http = require('http');
 const server = require('./lib/httpServer');
 
-
-
+var port = 8080
 server.listen(port, () => {
     console.log('server started on port: ', server.address().port);
 })
